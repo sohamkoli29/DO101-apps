0e8cf11ab4e5aaf359f55517a9b10b0dbddcf231
sohamkoli29 <sohamkoli29@gmail.com>
2025-01-18 18:50:09 +0530

    updated app to v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,11 +1,9 @@
-var express = require('express');
-app = express();
+var response;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
-});
+    response = 'This is version 2 of the app.' + '\n';
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+    //send the response to the client
+    res.send(response);
 
+});
